9edfac96e691c8f4c3990934f8c724343acf9ea5
MCCA01 <teckonn@crenergy.com.my>
2018-06-09 23:27:54 +0800

    Added vote POST back form and geolocation class.

--- polls/models.py
+++ polls/models.py
@@ -21,3 +21,10 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class GeolocationData(models.Model):
+    user_id = models.CharField(max_length=50)
+    customer_code = models.CharField(max_length=100)
+    customer_short_name = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
